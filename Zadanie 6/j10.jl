f5a9381c746d42014940e672d1401dfc7e7613cb
Arcticae <tomekgsd@gmail.com>
2018-06-22 13:48:02 +0200

    Finished the coures

--- Zadanie 6/j10.jl
+++ Zadanie 6/j10.jl
@@ -47,3 +47,42 @@ l = ReentrantLock()
     end
 end
 print("Główne zadanie\n")
+
+
+addprocs(3)
+
+nprocs() |> println
+workers() |> println
+nworkers() |> println
+
+futur=remotecall(myid,workers()[1])
+fetch(futur)
+remotecall(myid,workers()[1])
+
+cod=remotecall(()->rand(2,2),2) #fun,id,args
+cod2=@spawnat 3 fetch(cod)  #spawn fetch from previous on id 3
+fetch(cod2)     #fetch the fetch
+fetch(cod)
+
+
+###--------ZAD1--------####
+last=0
+function print_in_order(arg::Int64,repetitions::Int64)
+    for i = 1:repetitions
+        while((last)% 3 + 1 != arg)
+            yield()
+        end
+        print(arg,'\n')
+        global last=arg
+    end end
+
+@everywhere function fundef(args...)
+    print(args...)end
+
+
+t1=@task print_in_order(1,5)
+t2=@task print_in_order(2,5)
+t3=@task print_in_order(3,5)
+schedule(t3)
+schedule(t2)
+schedule(t1)
